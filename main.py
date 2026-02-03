be0fd2b45d1da102d631d461cc7311788a6dba50
Sri Darsan Sah <sridarsansah5907@gmail.com>
2026-02-03 18:46:32 +0530

    implemented modules for data validation and validation artifacts

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exceptions import NetworkSecurityError
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataValidationConfig
 
 import sys
 
@@ -13,6 +15,13 @@ if __name__=="__main__":
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("Initiate the Data Ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Completed data ingestion")
         print(data_ingestion_artifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityError(e, sys)
