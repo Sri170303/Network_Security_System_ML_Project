e19fbb2e57d7143f547769bade636aba4d5a465b
Sri Darsan Sah <sridarsansah5907@gmail.com>
2026-02-04 14:24:22 +0530

    add configs for data transformation

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -33,3 +33,19 @@ DATA_VALIDATION_VALID_DIR:str = "valid"
 DATA_VALIDATION_INVALID_DIR:str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE:str = "report.yaml"
+
+
+"""
+DATA TRANSFORMATION CONSTANTS
+"""
+DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform"
+}
+
+PREPROCESSING_OBJECT_FILE_NAME:str = "preprocessing.pkl"
