8ba4cfbdf81ea0bde8124a238703a775a5faf81a
Sri Darsan Sah <sridarsansah5907@gmail.com>
2026-02-04 14:27:48 +0530

    added lines to main.py to test transformation step

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exceptions import NetworkSecurityError
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.entity.config_entity import DataTransformationConfig
+
 
 import sys
 
@@ -23,5 +26,12 @@ if __name__=="__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+        logging.info("Starting data transformation")
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Completed data transformation")
     except Exception as e:
         raise NetworkSecurityError(e, sys)
+    
