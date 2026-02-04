a6e961c979e89b2b33726515e0ec855e68ab1556
Sri Darsan Sah <sridarsansah5907@gmail.com>
2026-02-04 14:28:48 +0530

    handled NoneType return of data validation artifacts

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -99,6 +99,7 @@ class DataValidation:
                 invalid_test_file_path=None,
                 drift_report_file_path=self.data_validation_config.drift_report_file_path
             )
+            return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityError(e, sys)
 
